56f2f9952a3505819eaf257fea8d94acf398a8b9
akim-muto <work.muto.akim.work@gmail.com>
2024-04-22 20:40:07 +0900

    test .env file and write file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
-import { Hono } from 'hono'
+import { Hono, HonoRequest } from "hono";
+import { env } from "hono/adapter";
+import { join } from "path";
+import { WindowsToaster } from "node-notifier";
+
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Hello Hono!');
 })
 
-export default app
+app.get('/create-or-open-ob', async (c) => {
+  const ob_f_name = c.req.query('ob_f_name');
+  if (!ob_f_name) {
+    return c.text('error: ob_f_name required.', 400);
+  }
+
+  const { OB_F_DIR } = env<{ OB_F_DIR: string }>(c);
+  const of_flle_full_path = join(OB_F_DIR,ob_f_name);
+  await Bun.write(of_flle_full_path, "");
+  return c.text(of_flle_full_path);
+})
+
+export default app;
